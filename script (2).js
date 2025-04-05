9d509bca2fbc00a5bf2ff53b9fc708d67dcedc9a
Annyshkaa <annyshkaa306@gmail.com>
2025-04-05 16:47:57 +0300

    11

--- script (2).js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('button');
-const heading = document.querySelector('h1'); 
-
-button.addEventListener('click', () => {
-    heading.textContent = 'Пока, мир'; 
-});
